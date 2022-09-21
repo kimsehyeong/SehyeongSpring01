5de53606d7358a84c1e826060701f2aceb2ad285
lego5 <lego5@kimSehyeong>
2022-09-21 15:48:18 +0900

    COMMIN 0921

--- SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
+++ SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
@@ -2,8 +2,11 @@ package kr.board.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import kr.board.entity.Board;
 
+@Mapper
 public interface BoardMapper {
 
 	public List<Board> getLists(); //전체 리스트
